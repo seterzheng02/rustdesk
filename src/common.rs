db9c64260b1c73085cce7d9725daceb335d57c81
seterzheng02@gmail.com <admin@RockyLinux.qqsq.lan>
2026-01-19 10:20:08 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rd.gzdxkj.com:21114".to_owned()
 }
 
 #[inline]
